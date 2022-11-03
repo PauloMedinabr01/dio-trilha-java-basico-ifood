8f81569801a7d325a1e67ab2ceacff9ac9c1fc1f
PauloMedinabr01 <paulomedinabr01@gmail.com>
2022-11-03 20:36:48 -0300

    POO visibilidade

---
+++ visibilidade/visibilidade/src/one/digitalinnovation/oo/Classe1.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.oo;
+
+/**
+ * Classe de exemplo para o exercício da Aula 5 de Orientação a Objetos.
+ */
+public class Classe1 {
+
+    private String atributo1; // Visto somente dentro da classe
+
+    protected String atributo2; // Visto dentro da classe, subclasse e mesmo pacote
+
+    public String atributo3; // Visto em qualquer lugar. Tudo também se aplica aos metodos.
+
+    private void metodo1() {
+
+    }
+
+    protected void metodo2() {
+
+    }
+
+    public void metodo3() {
+
+    }
+
+}
